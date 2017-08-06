6bac65574c699e9388f629b264c90047d165c6bd
Vu Pham <vutpham@outlook.com>
2017-08-05 17:23:55 -0700

    Add click event listeners to squares

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,17 @@ const colors = [
 ];
 
 const squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+const colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
+  //add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function () {
+    alert("clicked a square")
+  })
 }
